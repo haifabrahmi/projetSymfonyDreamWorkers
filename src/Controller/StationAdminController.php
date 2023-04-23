7c8069bd1808a325ceea893737bc8d55d7a4f923
oumaima <oumaima.benhamouda@esprit.tn>
2023-04-23 14:34:31 +0200

    station

---
+++ src/Controller/StationAdminController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Station;
+use App\Entity\Circuit;
+use App\Form\StationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
+
+#[Route('/admin/station')]
+class StationAdminController extends AbstractController
+{
+    #[Route('/', name: 'admin_station_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator, Request $request): Response
+    {
+        $pieChart = new PieChart();
+
+        $stations = $entityManager
+            ->getRepository(Station::class)
+            ->findAll();
+
+        $pagination = $paginator->paginate(
+            $stations, // Requête contenant les données à paginer (ici nos products)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            4 // Nombre de résultats par page
+        );
+
+        $circuits = $entityManager
+            ->getRepository(Circuit::class)
+            ->findAll();
+
+        $charts = [['Station', 'Number per Circuit']];
+
+        foreach ($circuits as $c) {
+            $circuitN = 0;
+            foreach ($stations as $s) {
+                if ($c == $s->getIdC()) {
+                    $circuitN++;
+                }
+            }
+
+            array_push($charts, [$c->getNomC(), $circuitN]);
+        }
+        
+        $pieChart->getData()->setArrayToDataTable($charts);
+
+        // dd($pieChart);
+
+        $pieChart->getOptions()->setTitle('Stations Number per Circuits');
+        $pieChart->getOptions()->setHeight(400);
+        $pieChart->getOptions()->setWidth(400);
+        $pieChart
+            ->getOptions()
+            ->getTitleTextStyle()
+            ->setColor('#07600');
+        $pieChart
+            ->getOptions()
+            ->getTitleTextStyle()
+            ->setFontSize(25);
+
+        return $this->render('stationAdmin/index.html.twig', [
+            'stations' => $pagination,
+            'piechart' => $pieChart,
+        ]);
+    }
+
+    #[Route('/new', name: 'admin_station_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $station = new Station();
+        $form = $this->createForm(StationType::class, $station);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $request->files->get('station')['image'];
+            $uploads_directory = $this->getParameter('uploads_directory');
+            $filename = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move($uploads_directory, $filename);
+            $station->setImage($filename);
+            $entityManager->persist($station);
+            $entityManager->flush();
+
+            $this->addFlash('notice','Ajout avec success!');
+
+            return $this->redirectToRoute('admin_station_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('stationAdmin/new.html.twig', [
+            'station' => $station,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idS}', name: 'admin_station_show', methods: ['GET'])]
+    public function show(Station $station): Response
+    {
+        return $this->render('stationAdmin/show.html.twig', [
+            'station' => $station,
+        ]);
+    }
+
+    #[Route('/{idS}/edit', name: 'admin_station_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Station $station, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(StationType::class, $station);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $request->files->get('station')['image'];
+            $uploads_directory = $this->getParameter('uploads_directory');
+            $filename = md5(uniqid()) . '.' . $file->guessExtension();
+            $file->move($uploads_directory, $filename);
+            $station->setImage($filename);
+            $entityManager->persist($station);
+            $entityManager->flush();
+
+            $this->addFlash('notice','Update avec success!');
+
+            return $this->redirectToRoute('admin_station_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('stationAdmin/edit.html.twig', [
+            'station' => $station,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idS}', name: 'admin_station_delete', methods: ['POST'])]
+    public function delete(Request $request, Station $station, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$station->getIdS(), $request->request->get('_token'))) {
+            $entityManager->remove($station);
+            $entityManager->flush();
+
+            $this->addFlash('notice','Delete avec success!');
+        }
+
+        return $this->redirectToRoute('admin_station_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Station.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Station
+ *
+ * @ORM\Table(name="station")
+ * @ORM\Entity(repositoryClass="App\Repository\StationRepository")
+ */
+class Station
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_s", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idS;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom_s", type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $nomS;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse_s", type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $adresseS;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ligne_s", type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $ligneS;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="horaire_s", type="date", nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $horaireS;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="equipement_s", type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $equipementS;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire_s", type="string", length=255, nullable=false)
+     * @Assert\NotBlank()
+     */
+    private $commentaireS;
+
+    /**
+     * @var \Circuit
+     *
+     * @ORM\ManyToOne(targetEntity="Circuit")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="idC", referencedColumnName="id_c")
+     * })
+     * @Assert\NotBlank()
+     */
+    private $idC;
+    
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="pos1", type="float", length=255, nullable=false)
+     */
+    private $pos1;
+    
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="pos2", type="float", length=255, nullable=false)
+     */
+    private $pos2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=false)
+     */
+    private $image;
+
+    public function getIdS(): ?int
+    {
+        return $this->idS;
+    }
+
+    public function getNomS(): ?string
+    {
+        return $this->nomS;
+    }
+
+    public function setNomS(string $nomS): self
+    {
+        $this->nomS = $nomS;
+
+        return $this;
+    }
+
+    public function getAdresseS(): ?string
+    {
+        return $this->adresseS;
+    }
+
+    public function setAdresseS(string $adresseS): self
+    {
+        $this->adresseS = $adresseS;
+
+        return $this;
+    }
+
+    public function getLigneS(): ?string
+    {
+        return $this->ligneS;
+    }
+
+    public function setLigneS(string $ligneS): self
+    {
+        $this->ligneS = $ligneS;
+
+        return $this;
+    }
+
+    public function getHoraireS(): ?\DateTimeInterface
+    {
+        return $this->horaireS;
+    }
+
+    public function setHoraireS(\DateTimeInterface $horaireS): self
+    {
+        $this->horaireS = $horaireS;
+
+        return $this;
+    }
+
+    public function getEquipementS(): ?string
+    {
+        return $this->equipementS;
+    }
+
+    public function setEquipementS(string $equipementS): self
+    {
+        $this->equipementS = $equipementS;
+
+        return $this;
+    }
+
+    public function getCommentaireS(): ?string
+    {
+        return $this->commentaireS;
+    }
+
+    public function setCommentaireS(string $commentaireS): self
+    {
+        $this->commentaireS = $commentaireS;
+
+        return $this;
+    }
+
+    public function getIdC(): ?Circuit
+    {
+        return $this->idC;
+    }
+
+    public function setIdC(?Circuit $idC): self
+    {
+        $this->idC = $idC;
+
+        return $this;
+    }
+    public function getPos1(): ?float
+    {
+        return $this->pos1;
+    }
+
+    public function setPos1(float $pos1): self
+    {
+        $this->pos1 = $pos1;
+
+        return $this;
+    }
+
+    public function getPos2(): ?float
+    {
+        return $this->pos2;
+    }
+
+    public function setPos2(float $pos2): self
+    {
+        $this->pos2 = $pos2;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+
+}
