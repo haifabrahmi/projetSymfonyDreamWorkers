9335964d38ebd65cfa60a23dfd6f122414132aa0
mohamed <mohamed.bouheni@esprit.tn>
2023-04-24 10:47:27 +0100

    resr

---
+++ src/Entity/Ticket.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TicketRepository::class)]
+class Ticket
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $prix = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_ticket = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getTypeTicket(): ?string
+    {
+        return $this->type_ticket;
+    }
+
+    public function setTypeTicket(string $type_ticket): self
+    {
+        $this->type_ticket = $type_ticket;
+
+        return $this;
+    }
+}
